20593c74319d0ab090aff7d11f5b06eabf5d264f
Hadasss <hadas.gadish@gmail.com>
2022-01-13 21:19:00 -0700

    bug fixes and final functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-var generateBtn = document.querySelector("#generate"); // button
-var passwordText = document.querySelector("#password"); // textarea text value
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 var upperCaseStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseStr = "abcdefghijklmnopqrstuvwxyz";
-var numStr = "1234567890";
+var numStr = "12345678901234567890";
 var specialCaseStr = '!"#$%&()*+,-.;:<>=?@[]_^`{}~|';
 var userChoice = "";
 var passLength = "";
 var passLengthNum = "";
 var passwordArr = [];
+var password = "";
 
-// prompt to get user input for password length
 var passwordLength = function () {
   passLength = prompt(
     "How long would you like your password to be?\nChoose between 8-128 characters."
@@ -17,13 +17,13 @@ var passwordLength = function () {
 
   if (passLength >= 8 && passLength <= 128) {
     passLengthNum = parseInt(passLength);
+    console.log(passLengthNum);
   } else {
     alert("Choose a number between 8-128!");
     passwordLength();
   }
 };
 
-// prompt to get user input to select caharacter types + variable to hold the value for reuse.
 var userInput = function () {
   var lowerCaseChoice = confirm("Would you like to use lowercase?");
   if (lowerCaseChoice) userChoice += lowerCaseStr;
@@ -36,44 +36,33 @@ var userInput = function () {
 
   var specialChoice = confirm("Would you like to use special characters?");
   if (specialChoice) userChoice += specialCaseStr;
+
+  console.log(userChoice);
 };
 
-// convert userChoice string to array and from it generate random character, for the length of passLength. then covert back to string.
 var randomArr = function () {
-  debugger;
-  // BUG need to reset for another button click
-  var password = "";
-
-  //   var userChoiceArr = [];
-  //   userChoiceArr = userChoice.split("");
-  //   console.log(userChoiceArr);
-
-  // BUG - generates numbers only and in pairs!
   for (var i = 0; i < passLengthNum; i++) {
-    var randomChar = [Math.floor(Math.random() * userChoice.length)];
+    var randomChar = userChoice[Math.floor(Math.random() * userChoice.length)];
     password += randomChar;
   }
   console.log(password);
   return password;
 };
 
-// function to trigger all other functions and combine the generating of character types.
 var generatePassword = function () {
   passwordLength();
-
   userInput();
-
   randomArr();
 };
 
 function writePassword() {
+  password = "";
   alert("Please choose the characteristics for your password");
-  // display in textarea
-  generatePassword(); // to displays generated password in textarea
+
+  generatePassword();
 
   passwordText.value = password;
   console.log(password);
-  //   passwordText = password;
 }
 
 generateBtn.addEventListener("click", writePassword);
